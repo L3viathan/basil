6c7adff8d954893e69f135325aa4e3e42ca73d33
L3viathan <git@l3vi.de>
2017-07-19 22:35:08 +0200

    Many changes
    - Body is now optional in out-lines
    - New primitive datatype: Literal. Also the base of topic and reply now
    - Literal has an overloaded % operator for string equality testing
    - Added constant & for reply id
    - Removed & and @ from possible Literal characters
    - Added None value, but no way to enter it in basil code

--- basil.py
+++ basil.py
@@ -4,9 +4,17 @@ from parser import parse_file
 from stdB import register as stdB
 
 
-def evaluate(ast_val, body):
+def evaluate(ast_val, body, ID):
+    # constant:
     if type(ast_val) == str:
-        return body
+        if ast_val == '@':
+            return body
+        elif ast_val == '&':
+            return ID
+        else:  # literal
+            return Literal(ast_val)
+    elif ast_val is None:
+        return None
     type_ = [*ast_val.keys()][0]
     if type_ == 'int':
         return int(ast_val['int'])
@@ -15,8 +23,16 @@ def evaluate(ast_val, body):
     elif type_ == 'string':
         return ast_val['string']
     elif type_ == 'tuple':
-        return tuple([evaluate(element, body) for element in ast_val['tuple']])
+        return tuple([evaluate(element, body, ID) for element in ast_val['tuple']])
     raise RuntimeError()
+
+class Literal:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return '<' + self.name + '>'
+    def __mod__(self, other):
+        return self.name == other
 
 
 class Basil:
@@ -28,19 +44,19 @@ class Basil:
     def do(self, listener, topic, body, ID):
         """Process a non-native actor"""
         for out in listener:
-            topic = out['topic']
-            eval_body = evaluate(out['body'], body)
-            reply = out['reply']
+            topic = Literal(out['topic'])
+            eval_body = evaluate(out['body'], body, ID)
+            reply = Literal(out['reply'])
             self.send(topic, eval_body, reply)
 
     def send(self, topic, body, reply=None):
         self.Q.add((topic, body, reply))
 
     def run(self):
-        self.Q.add(('main', None, None))
+        self.Q.add((Literal('main'), None, None))
         while self.Q:
             topic, body, ID = self.Q.pop()
-            for listener in self.listeners[topic]:
+            for listener in self.listeners[topic.name]:
                 if callable(listener):
                     listener(self, topic, body, ID)
                 else:
--- stdB.py
+++ stdB.py
@@ -2,45 +2,45 @@ import sys
 
 
 def _stdlib(self, topic, body, ID):
-    if topic == "cat":
+    if topic % "cat":
         self.send(ID, "".join(body))
-    elif topic == "print":
-        print(body)
-    elif topic == "read":
-        i = input(body)
+    elif topic % "print":
+        print(body if body is not None else "")
+    elif topic % "read":
+        i = input(body if body is not None else "")
         self.send(ID, i)
-    elif topic == "int":
+    elif topic % "int":
         self.send(ID, int(body))
-    elif topic == "float":
+    elif topic % "float":
         self.send(ID, float(body))
-    elif topic == "str":
+    elif topic % "str":
         self.send(ID, float(body))
-    elif topic == "<":
+    elif topic % "<":
         self.send(ID, body[0] < body[1])
-    elif topic == ">":
+    elif topic % ">":
         self.send(ID, body[0] > body[1])
-    elif topic == "==":
+    elif topic % "==":
         self.send(ID, body[0] == body[1])
-    elif topic == "!=":
+    elif topic % "!=":
         self.send(ID, body[0] != body[1])
-    elif topic == "<=":
+    elif topic % "<=":
         self.send(ID, body[0] <= body[1])
-    elif topic == ">=":
+    elif topic % ">=":
         self.send(ID, body[0] >= body[1])
-    elif topic == "?":
+    elif topic % "?":
         cond, one, two = body
         if cond:
             self.send(one, body)
         else:
             self.send(two, body)
-    elif topic == "err":
+    elif topic % "err":
         print("ERROR:", body, file=sys.stderr)
         sys.exit(1)
-    elif topic == "+":
+    elif topic % "+":
         self.send(ID, body[0] + body[1])
-    elif topic == "-":
+    elif topic % "-":
         self.send(ID, body[0] - body[1])
-    elif topic == "*":
+    elif topic % "*":
         self.send(ID, body[0] * body[1])
 
 
